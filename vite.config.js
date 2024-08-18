d221143f37daffb74abe6d1329ddaf9883214889
praharsh27 <praharsh.halpati2002@gmail.com>
2024-08-18 09:55:31 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';  // Ensure this import is correct
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/intern-project/',
-})
+  base: '/Project-One/',  // Correct base path for GitHub Pages
+});
